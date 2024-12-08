a25792bea57bd6d7b3e38082cbd0a0b800a511f9
Fabio Caldas <fabiocaldas266@gmail.com>
2024-12-08 15:05:34 -0300

    feat: start monitoring func

--- hello.go
+++ hello.go
@@ -1,26 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
 
-func startMonitoring() {
-	println("Monitoring started")
-}
+const websitesFileName = "websites.txt"
+const logsFileName = "logs.txt"
+const requestDelay = 5 * time.Second
 
-func showLogs() {
-	println("Showing Logs")
+func handleConfigAndLogsFiles() {
+	websitesFile, _ := os.Open(websitesFileName)
+
+	if websitesFile == nil {
+		_, err := os.Create(websitesFileName)
+
+		if err != nil {
+			println(err)
+			os.Exit(1)
+		}
+	}
+
+	logsFile, _ := os.Open(logsFileName)
+
+	if logsFile == nil {
+		_, err := os.Create(logsFileName)
+
+		if err != nil {
+			println(err)
+			os.Exit(1)
+		}
+	}
 }
 
 func main() {
-	println("Monitoring System")
-	println("1- Start monitoring")
-	println("2- Show logs")
-	println("3- End system")
+	handleConfigAndLogsFiles()
 
-	endSystem := false
-
-	for !endSystem {
-		var optionInputed int
-		fmt.Scan(&optionInputed)
+	for {
+		showMenu()
+		optionInputed := getOption()
 
 		switch optionInputed {
 		case 1:
@@ -29,10 +51,87 @@ func main() {
 			showLogs()
 		case 3:
 			println("Ending system...")
-			println("System ended")
-			endSystem = true
+			break
 		default:
 			println("Invalid option. Try again")
 		}
 	}
+}
+
+func showMenu() {
+	println("Monitoring System")
+	println("1- Start monitoring")
+	println("2- Show logs")
+	println("3- End system")
+}
+
+func startMonitoring() {
+	websiteFileInfo, _ := os.Stat(websitesFileName)
+
+	if websiteFileInfo == nil {
+		println(websitesFileName, "not founded, try open program again.")
+		os.Exit(1)
+	}
+
+	if websiteFileInfo.Size() == 0 {
+		println("Empty websites file, is required at least 1 website")
+		return
+	}
+
+	logsFileInfo, _ := os.Stat(logsFileName)
+
+	if logsFileInfo == nil {
+		println(logsFileName, "not founded, try open program again.")
+		os.Exit(1)
+	}
+
+	websitesFile, _ := os.Open(websitesFileName)
+	defer websitesFile.Close()
+
+	sc := bufio.NewScanner(websitesFile)
+	websites := []string{}
+
+	for sc.Scan() {
+		line := strings.TrimSpace(sc.Text())
+		websites = append(websites, line)
+	}
+
+	logsFile, _ := os.OpenFile(logsFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	defer logsFile.Close()
+
+	for {
+		for _, website := range websites {
+			res, err := http.Get(website)
+
+			if err != nil {
+				println("Invali URL:", string(website))
+				os.Exit(1)
+			}
+
+			var websiteStatus string
+
+			if res.StatusCode == 200 {
+				websiteStatus = "ONLINE"
+			} else {
+				websiteStatus = "OFFLINE"
+			}
+
+			logLine := "(" + website + "): " + websiteStatus
+			println(logLine)
+
+			currentDateTime := time.Now().Format("02/01/2006 15:04:05")
+			logsFile.WriteString("[" + currentDateTime + "]: " + logLine + "\n")
+		}
+
+		time.Sleep(requestDelay)
+	}
+}
+
+func showLogs() {}
+
+func getOption() int {
+	var optionInputed int
+	fmt.Scan(&optionInputed)
+
+	return optionInputed
 }
